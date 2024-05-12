0d53c1f9f654520b87e4dfd45f0bd6b4b6b20123
Koushik Ruidas <koushikruidas@gmail.com>
2024-05-12 13:27:10 +0530

    bug fix

--- userService/src/main/java/com/registar/hotel/userService/repository/RoomRepository.java
+++ userService/src/main/java/com/registar/hotel/userService/repository/RoomRepository.java
@@ -13,12 +13,15 @@ import java.util.List;
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
     List<Room> findByHotelId(Long id);
-    @Query(value = "SELECT * FROM room r " +
+    @Query(value = "SELECT r.* FROM room r " +
             "WHERE r.hotel_id IN (:hotelIds) " +
-            "AND NOT EXISTS (SELECT 1 FROM room_availability ra " +
-            "                WHERE ra.room_id = r.id " +
-            "                AND ra.booking_map_key BETWEEN :startDate AND :endDate " +
-            "                AND ra.is_booked = true)", nativeQuery = true)
+            "AND NOT EXISTS (" +
+            "    SELECT 1 FROM booking b " +
+            "    JOIN booking_room br ON b.id = br.booking_id " +
+            "    WHERE br.room_id = r.id " +
+            "    AND b.check_in_date <= :endDate " +
+            "    AND b.check_out_date >= :startDate" +
+            ")", nativeQuery = true)
     List<Room> findAvailableRoomsForDateRange(LocalDate startDate, LocalDate endDate, List<Long> hotelIds);
 
     @Query(value = "SELECT r.* FROM room r " +
