6d8735a918d5f22099a05d2b8954c7e8b0d07440
Koushik Ruidas <koushikruidas@gmail.com>
2024-06-11 14:40:55 +0530

    changed status code to 401 for access denied

--- userService/src/main/java/com/registar/hotel/userService/securityUtil/CustomAuthenticationEntryPoint.java
+++ userService/src/main/java/com/registar/hotel/userService/securityUtil/CustomAuthenticationEntryPoint.java
@@ -13,6 +13,6 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, IOException {
-        response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied");
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access Denied");
     }
 }
