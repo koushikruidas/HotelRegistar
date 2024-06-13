6342c42cf8354aef0b3bc7d658a5ce809e70ec0c
Koushik Ruidas <koushikruidas@gmail.com>
2024-06-13 14:17:32 +0530

    added validation

--- userService/src/main/java/com/registar/hotel/userService/model/request/AddressDTO.java
+++ userService/src/main/java/com/registar/hotel/userService/model/request/AddressDTO.java
@@ -1,12 +1,22 @@
 package com.registar.hotel.userService.model.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class AddressDTO {
+    @NotBlank(message = "Street cannot be blank")
     private String street;
+
+    @NotBlank(message = "City cannot be blank")
     private String city;
+
+    @NotBlank(message = "State cannot be blank")
     private String state;
+
+    @NotBlank(message = "Zip code cannot be blank")
     private String zipCode;
+
+    @NotBlank(message = "Country cannot be blank")
     private String country;
 }
